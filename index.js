e57d53653a4665fc36f88ce1fd6eafb573b51c7d
legacy-collaborator-7770 <johnvasiliadis@me.com>
2019-01-14 18:27:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function increaseRankBy(n) {
   const lis = document.querySelectorAll('ul.ranked-list li');
 
   for (let i = 0; i < lis.length; i++) {
-    lis[i].innerHTML = (parseInt(i) + n)
+    lis[i].innerHTML = parseInt(i + n)
   }
 }
