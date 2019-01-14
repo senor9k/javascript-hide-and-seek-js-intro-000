22d660bb30c2a72811cdfd35368c8951ca0a500c
legacy-collaborator-7770 <johnvasiliadis@me.com>
2019-01-14 18:24:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ function getFirstSelector(selector){
 function nestedTarget(nested, target) {
   return document.querySelector('div#nested div.target')
 }
+
+function increaseRankBy(n) {
+  const lis = document
+    .getElementById('app')
+    .querySelectorAll('ul.ranked-list li');
+
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = (i + 1).toString();
+  }
+}
