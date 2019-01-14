63745a8057ae5931a2c4a17be4524aa43be5310c
legacy-collaborator-7770 <johnvasiliadis@me.com>
2019-01-14 18:35:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ function nestedTarget(nested, target) {
 }
 
 function increaseRankBy(n) {
-  const lis = document.querySelectorAll('ul.ranked-list li');
-
+  const lis = document
+    .getElementById('app')
+    .querySelectorAll('ul.ranked-list li');
+    
   for (let i = 0; i < lis.length; i++) {
     lis[i].innerHTML = (i + n).toString()
   }
