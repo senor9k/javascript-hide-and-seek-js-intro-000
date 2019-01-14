ba8c78b4725870003a66d6f234f04a82c4bdd3e4
legacy-collaborator-7770 <johnvasiliadis@me.com>
2019-01-14 18:45:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(nested, target) {
+  return document.querySelector('div#nested div.target')
+}
+
+function increaseRankBy(n) {
+  const lis = document
+    .getElementById('app')
+    .querySelectorAll('ul.ranked-list li');
+
+  for (let i = 0; i < lis.length; i++) {
+    let newInnerHTML = parseInt(lis[i].innerHTML) + n
+    lis[i].innerHTML = newInnerHTML.toString()
+  }
+}
+
+function deepestChild(){
+  return document.querySelector('div#grand-node div div div div')
+}
