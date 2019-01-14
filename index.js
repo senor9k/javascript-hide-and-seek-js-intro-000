83f7b2a9b4c64519d2372312546db32a49355873
legacy-collaborator-7770 <johnvasiliadis@me.com>
2019-01-14 18:25:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ function nestedTarget(nested, target) {
 }
 
 function increaseRankBy(n) {
-  const lis = document
-    .getElementById('app')
-    .querySelectorAll('ul.ranked-list li');
+  const lis = document.querySelectorAll('ul.ranked-list li');
 
   for (let i = 0; i < lis.length; i++) {
     lis[i].innerHTML = (i + 1).toString();
